8c459f2de98318c85367282ab46d02695272b5ea
William Costa <dev.williamcosta@gmail.com>
2020-01-27 13:58:19 -0300

    Adjust icons components

---
+++ src/icons/index.ts
@@ -0,0 +1,79 @@
+import { Alert } from './Alert';
+import { ArrowArrange } from './ArrowArrange';
+import { ArrowDown } from './ArrowDown';
+import { ArrowDownIn } from './ArrowDownIn';
+import { ArrowDownLeft } from './ArrowDownLeft';
+import { ArrowDownLeftCircle } from './ArrowDownLeftCircle';
+import { ArrowDownRight } from './ArrowDownRight';
+import { ArrowLeft } from './ArrowLeft';
+import { ArrowReturn } from './ArrowReturn';
+import { ArrowRight } from './ArrowRight';
+import { ArrowSelect } from './ArrowSelect';
+import { ArrowUp } from './ArrowUp';
+import { ArrowUpLeft } from './ArrowUpLeft';
+import { ArrowUpOut } from './ArrowUpOut';
+import { ArrowUpRight } from './ArrowUpRight';
+import { ArrowUpRightCircle } from './ArrowUpRightCircle';
+import { Bank } from './Bank';
+import { BarCode } from './BarCode';
+import { Boletos } from './Boletos';
+import { Briefcase } from './Briefcase';
+import { Business } from './Business';
+import { Calendar } from './Calendar';
+import { Camera } from './Camera';
+import { CameraChange } from './CameraChange';
+import { CameraScan } from './CameraScan';
+import { CameraScanBarCode } from './CameraScanBarCode';
+import { Card } from './Card';
+import { Chart } from './Chart';
+import { Check } from './Check';
+import { ChevronDown } from './ChevronDown';
+import { ChevronLeft } from './ChevronLeft';
+import { ChevronRight } from './ChevronRight';
+import { ChevronRightSmall } from './ChevronRightSmall';
+import { ChevronUp } from './ChevronUp';
+import { Close } from './Close';
+import { Contacts } from './Contacts';
+import { Copy } from './Copy';
+import { Delete } from './Delete';
+
+export {
+  Alert,
+  ArrowArrange,
+  ArrowDown,
+  ArrowDownIn,
+  ArrowDownLeft,
+  ArrowDownLeftCircle,
+  ArrowDownRight,
+  ArrowLeft,
+  ArrowReturn,
+  ArrowRight,
+  ArrowSelect,
+  ArrowUp,
+  ArrowUpLeft,
+  ArrowUpOut,
+  ArrowUpRight,
+  ArrowUpRightCircle,
+  Bank,
+  BarCode,
+  Boletos,
+  Briefcase,
+  Business,
+  Calendar,
+  Camera,
+  CameraChange,
+  CameraScan,
+  CameraScanBarCode,
+  Card,
+  Chart,
+  Check,
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronRightSmall,
+  ChevronUp,
+  Close,
+  Contacts,
+  Copy,
+  Delete,
+};
