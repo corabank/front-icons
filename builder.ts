44794d722f0ac75c3fa98045b69bdfd5aae9db89
William Costa <dev.williamcosta@gmail.com>
2020-01-28 09:41:57 -0300

    Updated libs and install libs to builder index script

---
+++ builder.ts
@@ -0,0 +1,28 @@
+const fs = require('fs-extra');
+const glob = require('glob');
+const path = require('path');
+const util = require('util');
+
+const globAsync = util.promisify(glob);
+
+async function generateIndex() {
+  const files = await globAsync(path.join('./src/icons/', '*.tsx'));
+  const index = files
+    .map(file => {
+      const typename = path.basename(file).replace('.tsx', '');
+      return `export { default as ${typename} } from './icons/${typename}';\n`;
+    })
+    .join('');
+
+  await fs.writeFile(path.join('./src/', 'index.tsx'), index);
+}
+
+async function main() {
+  try {
+    await generateIndex();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
-// import postcss from 'rollup-plugin-postcss-modules'
-// import postcss from 'rollup-plugin-postcss'
 import resolve from '@rollup/plugin-node-resolve'
-// import url from 'rollup-plugin-url'
-// import svgr from '@svgr/rollup'
+import url from '@rollup/plugin-url';
+import svgr from '@svgr/rollup'
 
 import pkg from './package.json'
 
@@ -27,11 +25,8 @@ export default {
   ],
   plugins: [
     external(),
-    // postcss({
-    //   modules: true
-    // }),
-    // url(),
-    // svgr(),
+    url(),
+    svgr(),
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
