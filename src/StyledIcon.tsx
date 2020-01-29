19399a7c37212fc2947b71926b43116c8525df0b
William Costa <dev.williamcosta@gmail.com>
2020-01-29 16:59:06 -0300

    Added Functional props color feature

--- src/StyledIcon.tsx
+++ src/StyledIcon.tsx
@@ -2,13 +2,25 @@ import * as React from 'react';
 
 interface Props {
     size: string,
-    children: any
+    children: any,
 }
 
-const Icon: React.FunctionComponent<Props> = props => (
-  <svg>
-    ${props.children}
-  </svg>
-);
+enum Sizes {
+  small = 24,
+  medium = 36,
+  big = 48,
+}
+
+const Icon: React.FunctionComponent<Props> = props => {
+  return (
+    <svg height={Sizes[props.size]} width={Sizes[props.size]} viewBox="0 0 24 24" fill="none">
+      {props.children}
+    </svg>
+  );
+};
+
+Icon.defaultProps = {
+  size: 'small',
+}
 
 export default Icon;
