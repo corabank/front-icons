6dfd3168a25e2ba14942d984886baba8d9b153d1
William Costa <dev.williamcosta@gmail.com>
2020-05-08 18:39:22 -0300

    fix: path icon Flag

--- src/icons/Flag.tsx
+++ src/icons/Flag.tsx
@@ -6,15 +6,7 @@ import StyledIcon from '../StyledIcon';
 
 export const Flag: React.FunctionComponent<PropsInterface> = (props) => (
   <StyledIcon {...props}>
-    <path d="M1 11.9999C1 11.9999 1.875 11.2729 4.5 11.2729C7.125 11.2729 8.875 12.9999 11.5 12.9999C14.125 12.9999 15 12.0229 15 12.0229" stroke={props.color} stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M15 12.02V2" stroke={props.color} stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M1 18.9999V1.92993" stroke={props.color} stroke-linecap="round" stroke-linejoin="round"/>
-    <path
-      d="M1 1.932C1 1.932 1.875 1 4.5 1C7.125 1 8.875 2.727 11.5 2.727C14.125 2.727 15 2 15 2"
-      stroke={props.color}
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
+    <path d="M5 21V3.92999M19 14.02V4C19 4 18.125 4.727 15.5 4.727C12.875 4.727 11.125 3 8.5 3C5.875 3 5 3.932 5 3.932M5 14C5 14 5.875 13.273 8.5 13.273C11.125 13.273 12.875 15 15.5 15C18.125 15 19 14.023 19 14.023" stroke={props.color} stroke-linecap="round" stroke-linejoin="round"/>
   </StyledIcon>
 );
 
